7d68ea65805c69881acf6f37f9d93be26e379fb0
Joris Goosen <joris@jorisgoosen.nl>
2025-02-26 10:07:43 +0100

    secretely add dropLevelsType already here

--- columntype.h
+++ columntype.h
@@ -6,5 +6,5 @@ DECLARE_ENUM(columnType,				unknown = 0, scale = 1, ordinal = 2, nominal = 3, no
 DECLARE_ENUM(columnTypeChangeResult,	changed, cannotConvertStringValueToInteger, cannotConvertStringValueToDouble, cannotConvertDoubleValueToInteger, generatedFromAnalysis, unknownError);
 DECLARE_ENUM(computedColumnType,		notComputed, rCode, constructorCode, analysis, analysisNotComputed);
 DECLARE_ENUM(dbDbl,						nan, inf, neg_inf);
-
+DECLARE_ENUM(dropLevelsType,			noChoice = 0, drop = 1, keep = 2); /// noChoice means the user hasnt (implicitly) picked dropping over keeping levels.
 #endif // COLUMNTYPE_H
